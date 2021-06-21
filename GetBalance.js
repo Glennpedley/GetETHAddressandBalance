08e970660e176d61df7c9fc1628dea73eef707b7
Glennpedley <80082735+Glennpedley@users.noreply.github.com>
2021-06-21 10:48:01 +0800

    Create GetBalance.js
    This code is a slight improvement in that it extracts the main account address instead of me having to input the account address.

---
+++ GetBalance.js
@@ -0,0 +1,36 @@
+if (typeof window.ethereum !=='undefined') {
+    console.log('metamask is installed');
+    }else{
+    console.log('install metamask')
+    document.querySelector('.magicButton').innerHTML="<a href='https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn' target='_blank'> Install Metamask</a>";
+    document.querySelector('.intro').style.display='none'
+    }
+    
+    
+    const ethereumButton=document.querySelector('.magicButton');
+    const showAccount= document.querySelector('.thisIsMyAccountAddress');
+    const showBalance = document.querySelector('.showMyAccountBalance')
+    ethereumButton.addEventListener('click',()=> {
+
+    getAccount();
+    }); 
+    
+    async function getAccount(){
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts'});
+        const account = accounts[0];
+        showAccount.innerHTML=account;
+       
+    
+        const balance = await ethereum
+        .request ({
+        method:'eth_getBalance',
+        params:[account,"latest"],
+        });
+      
+        const read = parseInt(balance)/10**18;
+        console.log(read.toFixed(5));
+        showBalance.innerHTML=read.toFixed(8);
+        
+
+        }       
+    
